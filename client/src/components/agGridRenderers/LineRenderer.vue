2878416e0eb4b2306a7b64e5e88197de9122c56f
Jan Peřinka <jan.perinka@techfides.cz>
2024-08-22 14:08:25 +0200

    [TFPL-1087] Visual fix of json logs dialog

--- client/src/components/agGridRenderers/LineRenderer.vue
+++ client/src/components/agGridRenderers/LineRenderer.vue
@@ -3,6 +3,15 @@
     <q-chip v-if="params.data.info" square size="sm" color="orange-14">
       <q-icon name="info" size="15px" />
     </q-chip>
+    <q-chip
+      v-if="params.data.isJson"
+      clickable
+      square
+      size="sm"
+      color="primary"
+    >
+      <q-icon name="open_in_new" size="15px" @click="openDetail" />
+    </q-chip>
     <span v-if="!params.data.isJson" v-html="ansiHTML(params.value)" />
     <span v-else>{{ ansiHTML(params.value) }}</span>
   </div>
@@ -11,9 +20,22 @@
 <script lang="ts" setup>
 import type { ICellRendererParams } from "ag-grid-community";
 import ansiHTML from "ansi-html";
+import type { LogDetailDialogParams } from "@/components/logDetailDialog/logDetailDialogParam";
 
-type Props = { params: ICellRendererParams };
-defineProps<Props>();
+type CustomProps = {
+  showDialog: (params: LogDetailDialogParams) => void;
+  isJson: boolean;
+};
+
+type Props = { params: ICellRendererParams & CustomProps };
+const props = defineProps<Props>();
+
+const openDetail = () => {
+  props.params.showDialog({
+    title: "JSON log detail",
+    content: props.params.data.line,
+  });
+};
 </script>
 
 <style lang="scss" scoped>
