385a9c69c19f9930c70fb344b3d27b877848ed52
jludvik <jiri.ludvik@techfides.cz>
2025-01-09 20:38:59 +0100

    [TFPL-1252] refactored parallel running and fixed db reset indication with that

--- server/src/services/reset-defaults.service.ts
+++ server/src/services/reset-defaults.service.ts
@@ -1,90 +1,70 @@
 import { Injectable } from '@nestjs/common';
 import { CommandService, DefaultTask } from './command.service';
-import { ServicesService } from './services.service';
+import { BaseService, ServicesService } from './services.service';
 
 @Injectable()
 export class ResetDefaultsService {
+  private readonly copyEnvTaskName = 'COPY_ENV';
+  private readonly dbResetTaskName = 'RESET_DATABASE';
+
   constructor(
     private readonly commandService: CommandService,
     private readonly servicesService: ServicesService,
   ) {}
 
   async resetAllServices(gitCheckoutType: string): Promise<string> {
-    // reset repozitářů (clone pokud neexistují)
-    console.log('started procedure');
-    const serviceStatuses = this.servicesService.getServicesStatus();
-    const clonePromises: Promise<string>[] = [];
-    const resetPromises: Promise<string>[] = [];
-    const checkoutPromises: Promise<string>[] = [];
-    const pullPromises: Promise<string>[] = [];
+    const services = this.servicesService.getServices();
+    const promises = services.map((service) => {
+      this.resetService(service, gitCheckoutType);
+    });
 
-    for (const service of serviceStatuses) {
-      if (!service.cloned) {
-        clonePromises.push(
-          this.commandService.runTask(DefaultTask.GIT_CLONE, service.name, {
-            gitCheckoutType: gitCheckoutType,
-          }),
-        );
-        continue;
-      }
-
-      if (service.currentGitBranchHasChanges) {
-        resetPromises.push(
-          this.commandService.runTask(DefaultTask.GIT_RESET, service.name, {}),
-        );
-      }
-
-      checkoutPromises.push(
-        this.commandService.runTask(DefaultTask.GIT_CHECKOUT, service.name, {}),
-      );
-      pullPromises.push(
-        this.commandService.runTask(DefaultTask.GIT_PULL, service.name, {}),
-      );
-    }
-
-    await Promise.all(clonePromises);
-    await Promise.all(resetPromises);
-    await Promise.all(checkoutPromises);
-    await Promise.all(pullPromises);
-
-    //npm install
-    const npmInstallPromises: Promise<string>[] = [];
-    for (const service of serviceStatuses) {
-      npmInstallPromises.push(
-        this.commandService.runTask(DefaultTask.NPM_INSTALL, service.name, {}),
-      );
-    }
-    await Promise.all(npmInstallPromises);
-
-    //vykopírování envů / db reset
-    const copyEnvTaskName = 'COPY_ENV';
-    const envPromises: Promise<string>[] = [];
-    const dbResetTaskName = 'RESET_DATABASE';
-    const dbResetPromises: Promise<string>[] = [];
-
-    for (const service of this.servicesService.getServices()) {
-      if (service.genericTasks.includes(copyEnvTaskName)) {
-        envPromises.push(
-          this.commandService.runTask(copyEnvTaskName, service.name, {}),
-        );
-      }
-
-      if (service.genericTasks.includes(dbResetTaskName)) {
-        dbResetPromises.push(
-          this.commandService.runTask(dbResetTaskName, service.name, {}),
-        );
-      }
-    }
-
-    await Promise.all(envPromises);
-    console.log(`awaiting env promises: count: ${envPromises.length}`);
-
-    await new Promise((resolve) => setTimeout(resolve, 300));
-    await Promise.all(dbResetPromises);
-    console.log(`awaiting dbreset promises: count: ${dbResetPromises.length}`);
-
-    //nějak notifikuje
-    console.log('everything done');
+    await Promise.all(promises);
     return 'OK';
+  }
+
+  async resetService(
+    service: BaseService,
+    gitCheckoutType: string,
+  ): Promise<string> {
+    await this.resetServiceGit(service.name, gitCheckoutType);
+    await this.commandService.runTask(
+      DefaultTask.NPM_INSTALL,
+      service.name,
+      {},
+    );
+
+    if (service.genericTasks.includes(this.copyEnvTaskName)) {
+      await this.commandService.runTask(this.copyEnvTaskName, service.name, {});
+    }
+
+    if (service.genericTasks.includes(this.dbResetTaskName)) {
+      await this.commandService.runTask(this.dbResetTaskName, service.name, {});
+    }
+
+    return 'OK';
+  }
+
+  async resetServiceGit(
+    serviceName: string,
+    gitCheckoutType: string,
+  ): Promise<void> {
+    const serviceStatus = this.servicesService
+      .getServicesStatus()
+      .find((s) => s.name === serviceName);
+
+    if (!serviceStatus.cloned) {
+      await this.commandService.runTask(DefaultTask.GIT_CLONE, serviceName, {
+        gitCheckoutType: gitCheckoutType,
+      });
+      return;
+    }
+
+    await this.commandService.runTask(DefaultTask.GIT_RESET, serviceName, {});
+    await this.commandService.runTask(
+      DefaultTask.GIT_CHECKOUT,
+      serviceName,
+      {},
+    );
+    await this.commandService.runTask(DefaultTask.GIT_PULL, serviceName, {});
   }
 }
